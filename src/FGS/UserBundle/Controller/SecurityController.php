cdc03ae488bef8c452dcf04c7ed72e523a3db70d
PetitGrigri <fgriselles@hotmail.fr>
2017-02-27 11:20:04 +0100

    Suppression surcharge inutile FosUserBundle

--- src/FGS/UserBundle/Controller/SecurityController.php
+++ src/FGS/UserBundle/Controller/SecurityController.php
@@ -83,14 +83,4 @@ class SecurityController extends Controller
     {
         return $this->render('FOSUserBundle:Security:login.html.twig', $data);
     }
-
-    public function checkAction()
-    {
-        throw new \RuntimeException('You must configure the check path to be handled by the firewall using form_login in your security firewall configuration.');
-    }
-
-    public function logoutAction()
-    {
-        throw new \RuntimeException('You must activate the logout in your security firewall configuration.');
-    }
 }
