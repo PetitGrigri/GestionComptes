16ca1f60a2287e8a38aec5cca8caf24cb524d453
PetitGrigri <fgriselles@hotmail.fr>
2017-02-27 21:58:07 +0100

    insight

--- src/FGS/UserBundle/Controller/SecurityController.php
+++ src/FGS/UserBundle/Controller/SecurityController.php
@@ -14,7 +14,6 @@ namespace FGS\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Core\SecurityContextInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 class SecurityController extends Controller
@@ -73,4 +72,13 @@ class SecurityController extends Controller
         return $this->render('FOSUserBundle:Security:login.html.twig', $data);
     }
 
+    public function checkAction()
+    {
+        throw new \RuntimeException('You must configure the check path to be handled by the firewall using form_login in your security firewall configuration.');
+    }
+
+    public function logoutAction()
+    {
+        throw new \RuntimeException('You must activate the logout in your security firewall configuration.');
+    }
 }
